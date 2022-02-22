c70c0bd3501453c0f97d6a244c75c60033a913f9
MattiaPortanti <mattia.portantipm@gmail.com>
2022-02-22 16:07:43 +0100

    definizione dei km

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 let howManyKm = prompt("Quanti km vuoi percorrere a bordo del treno?")
 console.log(howManyKm);
-document.getElementById(my-distance).innerHTML += howManyKm;
+document.getElementById("my-distance").innerHTML += howManyKm;
